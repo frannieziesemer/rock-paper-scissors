8a5261009ff2574f0a529db899316b2262d17822
frannie <frances.ziesemer@gmail.com>
2020-12-14 10:50:19 +0100

    created game function to allow user to play five times and return overall winner

--- script.js
+++ script.js
@@ -2,11 +2,8 @@
 //randomly return rock, paper or scissors
 //define items in array
 // generate a random number to choose from the array 
-function computerPlay() {
-    let item = ['rock', 'paper', 'scissors'];
-    let selection = Math.floor(Math.random() * item.length);
-    return item[selection];
-}
+
+
 
 // user will input a selection which will be played against the computer 
 // function will take two inputs - comptuerSelection + playerSelection 
@@ -16,34 +13,7 @@ function computerPlay() {
 // if computer is 'paper
 //must be case sensitive 
 
-const playerSelection = prompt('rock, paper or scissors?').toLocaleLowerCase();
-console.log(`player: ${playerSelection}`);
-const computerSelection = computerPlay();
-
-function playRound(computer, player) {
-    console.log(`computer: ${computer}`);
-    if(computer === player) {
-        return 'Tie!';
-    } else if (player === 'rock' && computer === 'paper') {
-        return 'You lose!';
-    } else if (player === 'rock' && computer === 'scissors') {
-        return 'You Win!';
-    } else if (player === 'paper' && computer === 'rock') {
-        return 'You Win!'; 
-    } else if (player === 'paper' && computer === 'scissors') {
-        return 'You lose!';
-    } else if (player === 'scissors' && computer === 'paper') {
-        return 'You Win!';
-    } else if (player === 'scissors' && computer === 'rock') {
-        return 'You Lose!';
-    } else {
-        return 'error';
-    }
-}
-
-//console.log(playRound(comptuerSelection, playerSelection));
-
-
+//console.log(playRound(computerSelection, playerSelection));
 
 
 // game() function to play a 5 round game that keeps score 
@@ -51,16 +21,78 @@ function playRound(computer, player) {
 // if player wins 3 or more time 'you win'
 // if it is a tie 3 or more times ' tie! '
 
-function game() {
-    console.log(playRound(computerSelection, playerSelection));
-    console.log(playRound(computerSelection, playerSelection));
-    console.log(playRound(computerSelection, playerSelection));
-    console.log(playRound(computerSelection, playerSelection));
-    console.log(playRound(computerSelection, playerSelection));
-    // if('You Win!' >= 3) {
-    //     console.log('you win out of three');
-    // }
 
+//while the game number is less that 6 reapeat the function call play round 
+
+
+function game() {
+    
+    let playerScore = 0;
+    let computerScore = 0;
+    let totalScore = playerScore + computerScore;
+    let totalGames = 0;
+    let i;
+    
+    for(i = 0; i < 5; i++) {
+        console.log(`total games: ${totalGames}`);
+
+        let playerSelection = playerPrompt();
+        console.log(`player: ${playerSelection}`);
+
+        let computerSelection = computerPlay();
+        console.log(`computer: ${computerSelection}`);
+        
+        
+        playRound(playerSelection, computerSelection);
+        console.log(`player: ${playerScore}`);
+        console.log(`computer: ${computerScore}`);
+    }
+
+    function computerPlay() {
+        let item = ['rock', 'paper', 'scissors'];
+        let selection = Math.floor(Math.random() * item.length);
+        // console.log(`computer: ${item[selection]}`);
+        return item[selection];
+    }
+    
+
+    function playerPrompt() {
+        return prompt('rock, paper or scissors?').toLowerCase();
+    }
+    
+
+    function playRound(computer, player) {
+        
+        if(computer === player) {
+            i--;
+            console.log( 'Tie! - rematch');
+        } else if ((player === 'rock' && computer === 'paper') || 
+            (player === 'paper' && computer === 'scissors') || 
+            (player === 'scissors' && computer === 'rock')) {
+                ++computerScore;
+                console.log('You lose!') ;
+        } else if ((player === 'rock' && computer === 'scissors') || 
+            (player === 'scissors' && computer === 'paper') || 
+            (player === 'paper' && computer === 'rock')) {
+                ++playerScore;
+                console.log('You Win!');
+        } else {
+            console.log('error');
+        }
+        ++totalGames;
+    }
+    
+    console.log(`total: ${totalScore}`);
+
+    if (computerScore > playerScore) {
+        return 'From five games Computer Wins';
+    }   else if (computerScore < playerScore) {
+        return 'From five games You win!';
+    }   else if (computerScore === playerScore) {
+        return 'from five games Tie!';
+    }   else { 
+        return 'error..';
+    }
 }
-   
+
 console.log(game());
